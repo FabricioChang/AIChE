0bf59643923bea9f920d6fd75b443452b5293343
FabricioChang <150054392+FabricioChang@users.noreply.github.com>
2025-06-18 23:54:53 -0500

    Fixed Firebase 2

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDdTyyWQYWMA3VJCNFJiNtdAYC7Rc9gI9k",
+  authDomain: "portal-aiche.firebaseapp.com",
+  databaseURL: "https://portal-aiche-default-rtdb.firebaseio.com",
+  projectId: "portal-aiche",
+  storageBucket: "portal-aiche.firebasestorage.app",
+  messagingSenderId: "153207665579",
+  appId: "1:153207665579:web:b76bdf9ed2c6ff500918ee"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form-contacto');
     const datosFormulario = document.getElementById('datos-formulario');
