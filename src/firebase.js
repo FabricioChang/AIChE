f4c0bcedc3cfed538ba8ef011031a75b7406a2ce
FabricioChang <150054392+FabricioChang@users.noreply.github.com>
2025-07-14 10:45:21 -0500

    Update DB data

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDdTyyWQYWMA3VJCNFJiNtdAYC7Rc9gI9k",
+  authDomain: "portal-aiche.firebaseapp.com",
+  databaseURL: "https://portal-aiche-default-rtdb.firebaseio.com",
+  projectId: "portal-aiche",
+  storageBucket: "portal-aiche.firebasestorage.app",
+  messagingSenderId: "153207665579",
+  appId: "1:153207665579:web:b76bdf9ed2c6ff500918ee"
 };
 
 const app = initializeApp(firebaseConfig);
